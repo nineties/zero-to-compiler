ae85ff7f9034059be3a823246a02737f1078d58f
Koichi Nakamura <koichi@idein.jp>
2026-01-03 20:32:05 +0900

    add strlen

--- plisp.fs
+++ plisp.fs
@@ -2065,6 +2065,7 @@ s" setcdr" :noname swap node>arg1 ! nil ; add-prim
 s" nil?" :noname nil = if Strue else nil then ; add-prim
 s" cons?" :noname node>type @ Ncons = if Strue else nil then ; add-prim
 s" sym?"  :noname node>type @ Nsymbol = if Strue else nil then ; add-prim
+s" strlen" :noname to-str strlen make-int ; add-prim
 s" print" :noname print-sexp nil ; add-prim
 s" type"  :noname to-str type nil ; add-prim
 s" parse" :noname ( str -- sexp )
